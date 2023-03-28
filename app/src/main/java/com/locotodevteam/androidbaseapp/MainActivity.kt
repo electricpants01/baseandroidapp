9faac287994525ea542141db5389f06a3129d5c5
Christian Torrico Avila <chtorrico@ecisolutions.com>
2023-03-28 14:16:50 -0400

    update build.gradle

--- app/src/main/java/com/locotodevteam/androidbaseapp/MainActivity.kt
+++ app/src/main/java/com/locotodevteam/androidbaseapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.locotodevteam.androidbaseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.locotodevteam.androidbaseapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
     }
 }
